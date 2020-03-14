e169a5e42b56e101dca5f2bfd87e93a969e5b971
jangojowells <gsm2224@gmail.com>
2020-03-14 18:34:44 +0000

    initialized spring project

---
+++ src/test/java/com/looom/loomservice/LoomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.looom.loomservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
